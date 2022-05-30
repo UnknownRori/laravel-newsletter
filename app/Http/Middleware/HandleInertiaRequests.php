e88d1ac1fde41b14f188596e521d983d4e70e039
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2022-05-31 05:23:40 +0700

    add shared data

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -37,7 +37,13 @@ class HandleInertiaRequests extends Middleware
     public function share(Request $request): array
     {
         return array_merge(parent::share($request), [
-            //
+            'auth' => fn () => $request->user() ? $request->user()->only('id', 'name') : null,
+            'flash' => function () use ($request) {
+                return [
+                    'success' => $request->session()->get('success'),
+                    'error' => $request->session()->get('error'),
+                ];
+            },
         ]);
     }
 }
