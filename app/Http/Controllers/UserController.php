e3774a35c6012a65391b3b7fe1edd0d4a7bc6791
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2022-05-31 19:07:48 +0700

    add authentication

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
@@ -13,7 +14,16 @@ class UserController extends Controller
 
     public function auth(Request $request)
     {
-        //
+        $credentials = $request->validate([
+            'name' => ['required', 'string', 'min:5'],
+            'password' => ['required', 'string', 'min:4'],
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            return redirect('/');
+        }
+
+        session()->flash('error', 'Invalid user credentials');
     }
 
     public function logout(Request $request)
