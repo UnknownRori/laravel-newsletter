4cc1af19089a4492fdb1dc53c148719f5e16b5fc
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2022-07-26 12:41:05 +0700

    🔨 Added Vite config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
     plugins: [
         laravel([
-            'resources/css/app.css',
             'resources/js/app.js',
         ]),
         vue({
